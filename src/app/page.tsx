6944901b4756e432d1cab513ca231a011c450868
Snarloff <snarloff@gmail.com>
2023-10-18 17:34:27 -0300

    feat(components): :lipstick: iniciando estilização do card dos produtos pt. 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="mx-auto max-w-7xl px-8 pt-8 xl:px-0">
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-6">
+        <div className="overflow-hidden rounded-lg bg-white shadow-lg">produto 1</div>
+        <div className="overflow-hidden rounded-lg bg-white shadow-lg">produto 2</div>
+        <div className="overflow-hidden rounded-lg bg-white shadow-lg">produto 3</div>
+        <div className="overflow-hidden rounded-lg bg-white shadow-lg">produto 4</div>
+      </div>
+    </div>
+  )
 }
